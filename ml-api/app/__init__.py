70b9a3977fc5289753643d127bfb43ad40edae6e
Am_chb 92 <aminechebah25@gmail.com>
2026-01-14 17:44:17 +0100

    fix prediction displayed problem, add cors policy in ml-api, change the variable in the returned json

--- ml-api/app/__init__.py
+++ ml-api/app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 from app.controller.predict_price import house_price
 from app.controller import main
 
 app = Flask(__name__)
+
+# Allow Angular dev server
+CORS(
+    app,
+    resources={r"/*": {"origins": ["http://localhost:4200"]}},
+    supports_credentials=False,
+)
+
 app.register_blueprint(main)
 app.register_blueprint(house_price)
 
